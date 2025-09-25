9cad74e61f5e1224e508cd50e47e3db72392deb5
engimali2015-prog <engimali2015@gmail.com>
2025-09-25 11:25:23 +0300

    enter

--- Project1/styles/scripts.js
+++ Project1/styles/scripts.js
@@ -1,4 +1,28 @@
-let _firstname="EmanAya";
-var firstname="e";
-console
-const firstname="a";
+function calc() {
+    let yourName=document.getElementById('your-name');
+    let country=document.getElementById('country');
+    let arrivelDate=document.getElementById('arrivle-date');
+    let depatDate=document.getElementById('departure-date');
+    let singleRoom=document.getElementById('single-room');
+    let doubleRoom=document.getElementById('double-room');
+    let sweet=document.getElementById('sweet');
+    let wifiService=document.getElementById('wifi-service');
+    let breakfastService=document.getElementById('breakfast-service');
+    let poolService=document.getElementById('pool-service');
+    let gymService=document.getElementById('gym-service');
+    let sum=0;
+    const roomvalue = document.querySelector('input[name="roomt"]:checked');
+    console.log(yourName.value);
+    console.log(country.value);
+    console.log(arrivelDate.value);
+    console.log(depatDate.value);
+    console.log(wifiService.value);
+    console.log(breakfastService.value);
+    console.log(poolService.value);
+    console.log(gymService.value);
+    if(singleRoom.checked)
+    {
+        alert("checked");
+    }
+
+}
